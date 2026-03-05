7c04890c2e7efdb4821a7891ae2deac8b1fc734c
Edvard Farrow <edvardfrrw@gmail.com>
2026-03-05 18:51:56 +0400

    fix: change type

--- bad_data.sql
+++ bad_data.sql
@@ -2,14 +2,13 @@
 INSERT INTO customers (id, name, country) 
 VALUES (101, '   Dirty Harry   ', 'usa');
 
--- Deduplication check
--- id = 601 but different qty
 INSERT INTO sales (id, customerid, productid, qty, updated_at) 
 VALUES (601, 101, 1, 5, CURRENT_TIMESTAMP);
 
+-- Zero Value
 INSERT INTO sales (id, customerid, productid, qty, updated_at) 
-VALUES (601, 101, 1, 15, CURRENT_TIMESTAMP);
+VALUES (602, 101, 1, 0, CURRENT_TIMESTAMP);
 
 -- Negative values
 INSERT INTO sales (id, customerid, productid, qty, updated_at) 
-VALUES (602, 101, 2, -50, CURRENT_TIMESTAMP);
+VALUES (603, 101, 2, -50, CURRENT_TIMESTAMP);
